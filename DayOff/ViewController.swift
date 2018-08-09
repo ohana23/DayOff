afbe3210ae9492b38260c63214ce29a4e9e34222
Danny Ohana <danny.ohana@knights.ucf.edu>
2018-08-09 14:33:23 +0300

    Initial Commit

---
+++ DayOff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DayOff
+//
+//  Created by Shimon Ohana on 8/9/18.
+//  Copyright © 2018 Daniel Ohana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
